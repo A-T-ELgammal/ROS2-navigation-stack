5ce59b96ab3c38acbf4a476b81e9592989c64c1b
ahmed <a.t.elgammal@gmail.com>
2024-12-19 00:11:39 +0200

    clone a repo and deploy trutlebot3 in ignition & rviz2

--- install/setup.zsh
+++ install/setup.zsh
@@ -24,6 +24,9 @@ _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/ahmed/projects/ROS2/project-workspace/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/ahmed/projects/ROS2/turtlebot3-navigation-ignition/navigation2_ignition_gazebo_turtlebot3/install"
+_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
